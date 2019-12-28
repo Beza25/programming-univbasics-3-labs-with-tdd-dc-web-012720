328d487364ddbce9bfd5429b8e20047123a2e1d0
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:40:37 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,9 +1,9 @@
 # Add your variables here
 
-first_number =3 
-second_number =5
+first_number = 3 
+second_number = 5
 
-puts sum = first_number + second_numbder
+puts sum = first_number + second_number
 puts difference = first_number - second_number
 puts product = first_number * second_number
 puts quotient = first_number / second_number
