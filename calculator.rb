30d8b3f7717753174d768acf5508ce1cb93df9d2
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:22:56 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,12 @@
 # Add your variables here
+first_number = 3
+second_number = 5
 
-def get_variable_from_file (first_number, second_number)
-   
-   first_number = get_variable_from_file('./calculator.rb', "first_number")
-   second_number = get_variable_from_file('./calculator.rb', "second_number")
-   
-  puts sum
+def calculator (first_number, second_number)
+   sum = first_number + second_number
+   difference = first_number - second_number
+   product = first_number * second_number
+   qoutient = first_number / second_number
 end
-  
-#get_variable_from_file(first_number, second_number)
+calculator(first_number, second_number)
   
