33d651dc28f55e58757a78cf252dc714e9ad5ee1
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:35:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@
 
 first_number = 3
 second_number = 5
+def calculator (first_number, second_number)
 puts sum = first_number + second_number
 puts difference = first_number - second_number
 puts product = first_number * second_number
