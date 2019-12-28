3cbaeae11a795ad5947844af9ff1f8cfd71e1ba3
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:30:44 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,11 +2,4 @@
 
 first_number = 3
 second_number = 5
-def calculator (first_number, second_number)
-   sum = first_number + second_number
-   difference = first_number - second_number
-   product = first_number * second_number
-  qoutient = first_number / second_number
-end
-calculator(first_number, second_number)
-  
+puts sum = first_number + second_number
