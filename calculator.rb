b70225dfb7c8d8113738e43fddc5f176518e9150
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:23:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
 # Add your variables here
-first_number = 3
-second_number = 5
+
 
 def calculator (first_number, second_number)
+  first_number = 3
+  second_number = 5
    sum = first_number + second_number
    difference = first_number - second_number
    product = first_number * second_number
