6a3aad11446ce3fc0a7adbd226060f3ea637db65
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:26:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,11 +3,10 @@
 first_number = 3
 second_number = 5
 def calculator (first_number, second_number)
-  
-   var sum = first_number + second_number
-   var difference = first_number - second_number
-   var product = first_number * second_number
-   var qoutient = first_number / second_number
+   sum = first_number + second_number
+   difference = first_number - second_number
+   product = first_number * second_number
+  qoutient = first_number / second_number
 end
 calculator(first_number, second_number)
   
