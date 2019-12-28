92f7006a5316ac530eb83531876ea72054b2de42
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:44:18 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,6 +2,7 @@
 
 first_number = 3 
 second_number = 5
+var sum, diffence,product,quotient
 def calculator ( first_number, second_number)
   puts sum 
   puts difference = first_number - second_number
