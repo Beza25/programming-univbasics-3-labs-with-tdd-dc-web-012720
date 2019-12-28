0e9e99cdcbe039260daad7d46e522573ab119dc9
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:13:07 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 
-def get_variable_from_file (first_number, second_number)
+def ./calculator.rb (first_number, second_number)
    
    #first_number = get_variable_from_file('./calculator.rb', "first_number")
    #second_number = get_variable_from_file('./calculator.rb', "second_number")
