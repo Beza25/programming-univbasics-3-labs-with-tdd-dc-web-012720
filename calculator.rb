b4f142a3185ddc40062d9ff25b6229d8044c427e
Beza Sirak <sirak.bezawit@gmail.com>
2019-12-28 22:10:14 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,8 +1,9 @@
 # Add your variables here
 var first_number, second_number, sum
 def get_variable_from_file (first_number, second_number)
-   first_number = get_variable_from_file('./calculator.rb', "first_number")
-   second_number = get_variable_from_file('./calculator.rb', "second_number")
+   
+   #first_number = get_variable_from_file('./calculator.rb', "first_number")
+   #second_number = get_variable_from_file('./calculator.rb', "second_number")
    
   if (second_number != 0)
     sum = first_number + second_number
